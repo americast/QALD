32d6d7c5e9492a6052c9f824e3590cd828d4120e
americast <sayan.sinha@iitkgp.ac.in>
2018-09-30 10:51:56 +0530

    Another ner

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo pip3 install -r req.txt
 sudo python3 -m spacy download en
-python3 -c "import nltk; nltk.download('punkt')"
+python3 -c "import nltk; nltk.download('punkt'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words')"
 mkdir -p data
 
