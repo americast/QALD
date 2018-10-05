34da709b95584b3ad78fc799146adccc2cbd085c
americast <sayan.sinha@iitkgp.ac.in>
2018-10-05 09:11:19 +0530

    Add proxy to setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo pip3 install -r req.txt
+sudo pip3 --proxy "http://172.16.2.30:8080" install -r req.txt
 sudo python3 -m spacy download en
 python3 -c "import nltk; nltk.download('punkt'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words')"
 mkdir -p data
